231de8a2d2c2b57ca5d403ea7d07c20a63e8e362
4mh23cs018-design <4mh23cs018@gmail.com>
2026-02-13 14:25:37 +0530

    connnnn

--- db.py
+++ db.py
@@ -1,4 +1,3 @@
-
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -6,16 +5,17 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-
-Base=declarative_base()
+Base=declarative_base() 
 
 DATABASE_URL=os.getenv("DATABASE_URL")
 print("DATABASE_URL",DATABASE_URL)
-
+#create engine once at module level
 engine=create_engine(DATABASE_URL)
-SessionLocal=sessionmaker(autocommit=False,autoflush=False,bind=engine)
+
+#create sessionaLocal class for creatimg database sessions
+SessionLocal=sessionmaker(autocommit=False, autoflush=False, bind=engine)
 def get_db():
-    db = SessionLocal()
+    db=SessionLocal()
     try:
         yield db
     finally:
